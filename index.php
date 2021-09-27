2605d1f6887174462c0ca30191d76b22ce93e1ba
bogdana29 <bogdana29@yahoo.com>
2021-09-27 14:29:48 +0300

    greseala config si nu se commisese index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types = 1);
+error_reporting(E_ALL  & ~E_NOTICE & ~E_DEPRECATED & ~E_USER_DEPRECATED  & ~E_WARNING  );
+//ini_set('display_errors', true);
+
+include './include/autoloader.inc.php';
+require_once 'vendor/autoload.php';
+echo "Hero game"; 
+$game = new Game();
+$game->startGame();
